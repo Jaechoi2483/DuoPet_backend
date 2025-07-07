184c1a61feaa3953caec862cfbff8aa7d2386c47
Choi Jae Myoung <jaechoi79@nate.com>
2025-07-07 16:56:54 +0900

    백엔드첫커밋

---
+++ src/main/java/com/petlogue/duopetbackend/DuopetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.petlogue.duopetbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuopetBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuopetBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/petlogue/duopetbackend/DuopetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petlogue.duopetbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuopetBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
