8d956d02eb041933e0aff4a1a86d3cefd3f58af5
Starchatcher <snopia@naver.com>
2025-07-11 18:31:26 +0900

    보호소찾기 병원찾기 백엔드 작업

--- src/main/java/com/petlogue/duopetbackend/pet/jpa/repository/PetRepository.java
+++
@@ -1,7 +0,0 @@
-package com.petlogue.duopetbackend.pet.jpa.repository;
-
-import com.petlogue.duopetbackend.pet.jpa.entity.Pet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PetRepository extends JpaRepository<Pet, Long> {
-}
